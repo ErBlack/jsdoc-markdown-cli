1be476fa7c56834dc27257ca2aa2a1d6e8e1a0f1
ertema <ertema@yandex-team.ru>
2018-12-04 11:46:16 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,20 @@
+# jsdoc-markdown-cli
+
+```
+jsdoc-markdown-cli --help
+
+Usage: index [options] <patterns ...>
+
+Generates README.md
+
+Options:
+  -V, --version         output the version number
+  -p --preset [preset]  Filenames and paths preset
+  -d --dry-run          Don't create files
+  -c --clear-empty      Delete empty files
+  -h, --help            output usage information
+```
+
+## License
+
+© 2013 YANDEX LLC. The code is released under the [Mozilla Public License 2.0](LICENSE.txt).
---
+++ bin/index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+const commander = require('commander');
+const glob = require('glob');
+const builder = require('../lib/builder');
+
+const PRESETS = require('../lib/presets.json');
+
+commander
+    .version(require('../package').version)
+    .usage('[options] <patterns ...>')
+    .description('Generates README.md')
+    .option('-p --preset [preset]', 'Filenames and paths preset', String)
+    .option('-d --dry-run', 'Don\'t create files')
+    .option('-c --clear-empty', 'Delete empty files')
+    .parse(process.argv);
+
+const {
+    args,
+    dryRun,
+    clearEmpty,
+    preset = 'default'
+} = commander;
+
+if (!PRESETS[preset]) {
+    throw new Error(`Unknown preset ${preset}`);
+}
+
+const {
+    folders,
+    filename
+} = PRESETS[preset];
+
+const targets = args.reduce((paths, pattern) => [
+    ...paths,
+    ...folders.reduce((folders, suffix) => [
+        ...folders,
+        ...glob.sync(`${pattern}*${suffix}`)
+    ], [])
+], [])
+    .map(folder => ({
+        folder,
+        files: glob.sync(`${folder}${filename}`, { nodir: true })
+    }))
+    .filter(({ files }) => files.length);
+
+builder(targets, {
+    clearEmpty,
+    dryRun
+});
